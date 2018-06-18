cf9a7233cb9fe9023d6d6ec7a7792547fd33a0f3
elastic-rs <travis@elastic.rs>
2018-06-18 02:18:11 +0000

    Build docs at elastic-rs/elastic@6ca9103

---
+++ implementors/futures/stream/trait.Stream.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["elastic"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/futures/0.1/futures/stream/trait.Stream.html\" title=\"trait futures::stream::Stream\">Stream</a> for <a class=\"struct\" href=\"elastic/http/struct.AsyncHttpResponse.html\" title=\"struct elastic::http::AsyncHttpResponse\">AsyncHttpResponse</a>",synthetic:false,types:["elastic::http::async::AsyncHttpResponse"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
